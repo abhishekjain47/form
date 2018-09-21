e59d02fa1a82fe030866f422271e4233f55d4c4b
Abhishek jain <you@example.com>
2018-09-21 16:40:09 +0530

    form

---
+++ form/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from .forms import NameForm
+
+def get_name(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # redirect:
+            return HttpResponseRedirect('/thanks/')
+        else:
+            form = NameForm()
+
+    return render(request, 'name.html', {'form': form})
